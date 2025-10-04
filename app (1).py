63784acdeae53e957d4a69614f9a24b4ed2d38c1
mariajosebedoya208-droid <mariajosebedoya208@gmail.com>
2025-10-03 21:24:17 -0600

    Update app (1).py
    Corrección

--- app (1).py
+++ app (1).py
@@ -28,6 +28,6 @@ ticker=st.multiselect("Elija un ticker o varios", lista_tickers)
 # Boton
 
 if st.button("Descargar"):
-  data=yf.download(ticker, period="1mo")['close']
-  st.subheader('Precios de Cierre')
+  data=yf.download(ticker, period="1mo")["close"]
+  st.subheader("Precios de Cierre")
   st.dataframe(data)
